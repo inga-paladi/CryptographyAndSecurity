084ebb9e46f23d283053d6c1ff4c4722c3734c95
inga-paladi <ingapaladi98@gmail.com>
2023-10-31 20:48:42 +0200

    add lab no. 4

---
+++ CS_LAB_4/cs_lab_4/cs_lab_4/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Data.Common;
+
+class Program
+{
+    static int[,,] sBoxes = new int[8, 4, 16]
+    {
+    // s-box 1
+    {
+        {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+        {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+        {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
+        {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
+    },
+    // s-box 2
+    {
+        {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+        {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
+        {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
+        {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}
+    },
+    // s-box 3
+    {
+        {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+        {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
+        {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
+        {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}
+    },
+    // s-box 4
+    {
+        {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
+        {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
+        {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
+        {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14}
+    },
+    // s-box 5
+    {
+        {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
+        {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
+        {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
+        {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}
+    },
+    // s-box 6
+    {
+        {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
+        {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
+        {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
+        {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}
+    },
+    // s-box 7
+    {
+        {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
+        {13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
+        {1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
+        {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}
+    },
+    // s-box 8
+    {
+        {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
+        {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
+        {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
+        {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
+    }
+    };
+
+    static void Main()
+    {
+        // un random sa genereze 48 biti si sa afiseze la tastatura
+        // the random 48 bits should be devided in groups of 6 bits
+        // Generate 48 random bits
+        // Generate 48 random bits
+        Random random = new();
+        byte[] randomBytes = new byte[6];
+        random.NextBytes(randomBytes);
+
+        string bitsAsStr = new("");
+        foreach (byte oneByte in randomBytes)
+        {
+            bitsAsStr += Convert.ToString(oneByte, 2).PadLeft(8, '0');
+        }
+
+        Console.WriteLine("48 biti aleatori: " + bitsAsStr);
+
+        // Accept the group number from the user
+        int groupNumber = 0;
+        while (true)
+        {
+            Console.Write("Enter the group number (1-8): ");
+            if (int.TryParse(Console.ReadLine(), out groupNumber) && groupNumber >= 1 && groupNumber <= 8)
+                break;
+            else
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 8.");
+        }
+
+        int bitsPerGroup = 6;
+        int startingBitPosition = (groupNumber - 1) * bitsPerGroup;
+        var groupOfRequestedBits = bitsAsStr.Substring(startingBitPosition, bitsPerGroup);
+
+        var rowAsStrOfBits = new string(new[] { groupOfRequestedBits[0], groupOfRequestedBits[5] }); // the first and the last bits
+        var colAdStrOfBits = groupOfRequestedBits.Substring(1, 4); // the remaining bits, between 1 and 6
+
+        var resultRow = Convert.ToInt32(rowAsStrOfBits, 2);
+        var resultCol = Convert.ToInt32(colAdStrOfBits, 2);
+
+        var resultFromSBox = sBoxes[groupNumber - 1, resultRow, resultCol];
+        var resultFromSBoxAsStr = Convert.ToString(resultFromSBox, 2).PadLeft(4, '0');
+
+        Console.WriteLine($"Extracted 6 bits from group {groupNumber}: {groupOfRequestedBits}");
+        Console.WriteLine($"The result was taken from S-Box {groupNumber}, row {resultRow+1}, col {resultCol+1}, and is: {resultFromSBox} ({resultFromSBoxAsStr})");
+    }
+}
+
+
